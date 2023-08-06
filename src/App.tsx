f0c862ac0d79c119d95dbf21395dcef160897219
Mateus Bolito <user.email>
2023-08-06 14:17:12 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import Categories from "./components/Categories/Categories";
+import Gallery from "./components/Gallery/Gallery";
+import Menu from "./components/Menu/Menu";
+import Offers from "./components/Offers/Offers";
+import Search from "./components/Search/Search";
+
+function App() {
+  return (
+    <div>
+      <section className="bg-topbackground bg-no-repeat bg-cover min-h-[840px]  flex flex-col">
+        <header className="flex justify-between px-12 py-6">
+          <div className="">
+            <img src="./assets/logo.svg" alt="" />
+          </div>
+
+          <Menu />
+        </header>
+
+        <section className="px-28 py-24">
+          <h1 className="text-7xl text-white font-bold">
+            The whole world <br /> awaits.
+          </h1>
+
+          <div className="py-10">
+            <Search />
+          </div>
+
+          <Categories />
+        </section>
+
+        <section className="px-28 py-24">
+          <h1 className="text-3xl text-white font-bold">
+            Top Vacation Destinations
+          </h1>
+
+          <Gallery />
+        </section>
+      </section>
+
+      <section className="offers">
+        <div className="px-28">
+          <h1 className="text-black text-3xl font-bold pt-20">Offers</h1>
+
+          <Offers />
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'background-search': 'rgba(255, 255, 255, 0.4)',
+        'background-categories': 'rgba(18,18,20,0.8)',
+      },
+      backgroundImage: {
+        topbackground: 'url(./assets/top-background.jpeg)'
+      },
+
+    },
+    plugins: [],
+  }
+}
